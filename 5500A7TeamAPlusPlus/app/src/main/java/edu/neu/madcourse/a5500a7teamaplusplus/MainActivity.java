0274fccd1f0a4c4089cbe11d9ca8b5f87d271e40
aide4222 <ni.ru@northeastern.edu>
2022-03-23 19:55:24 -0700

    projectwithfirebase

---
+++ 5500A7TeamAPlusPlus/app/src/main/java/edu/neu/madcourse/a5500a7teamaplusplus/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.a5500a7teamaplusplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
